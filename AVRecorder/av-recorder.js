4d17d3f6586cff8aa30ccf37b0ea34f9c51071fd
carlb <burnstei@uoregon.edu>
2018-07-31 22:44:34 -0700

    OST-2 #time 30m #comment -Moved html for the recorder into the jquery plugin, so the html gets appended into the avrecorder's root element div. TODO: Eventually will make various classes and text for the recorder configurable through config params.  -Also removed video recorder from demo.php for Edge/IE.

--- AVRecorder/av-recorder.js
+++ AVRecorder/av-recorder.js
@@ -36,6 +36,64 @@
 	
         var $avRecorder = $('#' + id);
         var $avRecorderFallback =  $('#' + id + '-fallback-ajax-wrapper');
+        
+        //Template for recorder HTML
+        var recorderHtml = "<div class='av-recorder panel panel-primary'>\
+				<div class='panel-heading'>\
+					<h3 class='av-recorder-status panel-title'>Click 'Start' to\
+						enable your mic.</h3>\
+				</div>\
+				<div class='av-recorder-preview panel-body' style='display: none;'>\
+					<div class='loader' style='display: none;'></div>\
+					<canvas class='av-recorder-meter' style='display: none;'></canvas>\
+					<video class='av-recorder-video' style='display: none;'></video>\
+					<audio class='av-recorder-audio' controls='' style='display: none;'></audio>\
+				</div>\
+				<div class='av-recorder-progress progress' style='display: none;'>\
+					<div class='progress-bar' role='progressbar'></div>\
+				</div>\
+				<div class='panel-footer'>\
+					<div class='av-recorder-controls'>\
+						<button\
+							class='btn btn-primary btn-lg btn-block av-recorder-enable'\
+							title='Click to enable your mic.'>Start</button>\
+						<button\
+							class='btn btn-danger btn-lg btn-block av-recorder-record'\
+							title='Click to start recording.' style='display: none;'>\
+							<span class='glyphicon glyphicon-record'></span> Record\
+						</button>\
+						<button\
+							class='btn btn-success btn-lg btn-block av-recorder-play'\
+							title='Click to play recording.' style='display: none;'>\
+							<span class='glyphicon glyphicon-play'></span> Play\
+						</button>\
+						<button\
+							class='btn btn-warning btn-lg btn-block av-recorder-stop'\
+							title='Click to stop recording.' style='display: none;'>\
+							<span class='glyphicon glyphicon-stop'></span> Stop\
+						</button>\
+						<button\
+							class='btn btn-primary btn-lg btn-block av-recorder-settings'\
+							title='Click to access settings.'>\
+							<span class='glyphicon glyphicon-cog'></span> Settings\
+						</button>\
+						<button\
+							class='btn btn-primary btn-lg btn-block av-recorder-enable-audio'\
+							title='Click to enable your mic.' style='display: none;'>\
+							<span class='glyphicon glyphicon-headphones'></span> Audio\
+						</button>\
+						<button\
+							class='btn btn-success btn-lg btn-block av-recorder-enable-video'\
+							title='Click to enable your camera.' style='display: none;'>\
+							<span class='glyphicon glyphicon-facetime-video'></span> Video\
+						</button>\
+					</div>\
+				</div>\
+			</div>";
+        
+        //Insert the recorder's inner html to the root element.
+        $avRecorder.append(recorderHtml);
+        
         switch (recorderType) {
           case 'AvRecorder':
             $avRecorder.show();
